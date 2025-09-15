c3f82b038434636bc6d20121cf5a51e9514a17c3
Thierry Lévèque <tleveque@gmail.com>
2025-09-15 09:33:26 -0400

    Initial Commit

---
+++ ShowCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShowCard
+//
+//  Created by Thierry Leveque on 2025-09-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShowCard/ShowCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowCardApp.swift
+//  ShowCard
+//
+//  Created by Thierry Leveque on 2025-09-15.
+//
+
+import SwiftUI
+
+@main
+struct ShowCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
